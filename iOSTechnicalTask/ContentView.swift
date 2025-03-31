3b92707b8e36a3330d048ce9dd12ecb0e2b3ca71
Ambuj Singh <ambuj.singh.contractor@pepsico.com>
2025-03-31 17:46:25 +0530

    Initial Commit

---
+++ iOSTechnicalTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSTechnicalTask
+//
+//  Created by Ambuj Singh on 31/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSTechnicalTask/iOSTechnicalTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTechnicalTaskApp.swift
+//  iOSTechnicalTask
+//
+//  Created by Ambuj Singh on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSTechnicalTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
